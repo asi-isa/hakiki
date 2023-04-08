263256f786e0d948e00f927fcdd34c26969f14be
asi-isa <ali.inceoglu@protonmail.com>
2023-04-08 19:27:55 +0200

    traverse files in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-mod args;
-use std::{collections::HashMap, path::PathBuf};
-
-use args::Args;
 use chrono::{DateTime, Local};
+use rayon::prelude::*;
+use std::{
+    collections::HashMap,
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 use walkdir::WalkDir;
 
+mod args;
+use args::Args;
+
+#[derive(Debug)]
 struct FileInfo {
     name: String,
     path: PathBuf,
@@ -15,16 +21,19 @@ struct FileInfo {
 fn main() {
     let Args { path } = Args::get();
 
-    let mut map: HashMap<String, Vec<FileInfo>> = HashMap::new();
+    let map: Arc<Mutex<HashMap<String, Vec<FileInfo>>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    let map_clone = map.clone();
 
     let size_treshold = 100_000_000;
 
     WalkDir::new(path)
         .into_iter()
+        .par_bridge()
         .filter_map(|e| e.ok())
         .filter(|e| e.file_type().is_file())
         .filter(|e| e.metadata().unwrap().len() > size_treshold)
-        .for_each(|e| {
+        .for_each(move |e| {
             let name = String::from(e.file_name().to_string_lossy());
             let path = e.path().to_owned();
             let metadata = e.metadata().expect("couldn't extract metadata");
@@ -41,24 +50,31 @@ fn main() {
                 created,
             };
 
-            let map_entry = map.entry(name).or_insert(Vec::new());
-
-            map_entry.push(fi);
+            map_clone
+                .lock()
+                .unwrap()
+                .entry(name)
+                .or_insert(Vec::new())
+                .push(fi);
         });
 
-    map.into_iter().filter(|me| me.1.len() > 1).for_each(|me| {
-        println!("############################################");
-        let f_name = me.0;
-        println!("File: {f_name}\n");
+    map.lock()
+        .unwrap()
+        .iter()
+        .filter(|me| me.1.len() > 1)
+        .for_each(|me| {
+            println!("############################################");
+            let f_name = me.0;
+            println!("File: {f_name}\n");
 
-        me.1.iter().for_each(|fi| {
-            let date = fi.created.format("%Y-%m-%d %H:%M:%S").to_string();
+            me.1.iter().for_each(|fi| {
+                let date = fi.created.format("%Y-%m-%d %H:%M:%S").to_string();
 
-            println!("Name: {:?}", fi.name);
-            println!("Path: {:?}", fi.path.display());
-            println!("Created: {:?}", date);
-            println!("Bytes: {}", fi.size);
+                println!("Name: {:?}", fi.name);
+                println!("Path: {:?}", fi.path.display());
+                println!("Created: {:?}", date);
+                println!("Bytes: {}", fi.size);
+            });
+            println!("############################################\n");
         });
-        println!("############################################\n");
-    });
 }
